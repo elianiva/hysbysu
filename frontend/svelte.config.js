304cf5034b123a08b7e4fc4325001ad315e4f0d2
elianiva <dicha.arkana03@gmail.com>
2023-03-27 12:05:20 +0700

    chore: init frontend

---
+++ frontend/svelte.config.js
@@ -0,0 +1,16 @@
+import adapter from '@sveltejs/adapter-auto';
+import path from "node:path";
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter(),
+		alias: {
+			"~/*": path.resolve("./src")
+		}
+	},
+};
+
+export default config;
